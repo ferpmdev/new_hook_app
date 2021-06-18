696e8449811610141fd21ef947ebe155bda257f8
Fernando Pioli Martínez <ferpm.dev@gmail.com>
2021-06-18 07:53:38 -0300

    terminé la clase 113

--- src/components/04-useRef/FocusScreen.js
+++ src/components/04-useRef/FocusScreen.js
@@ -1,14 +1,22 @@
 import React, { useRef } from "react";
 
 export const FocusScreen = () => {
-  const ref = useRef(initialValue);
+  const inputRef = useRef();
   const handleClick = () => {
+    // document.querySelector("input").select();
+    inputRef.current.select();
+
     document.querySelector("input").select();
   };
   return (
     <div>
       <h1>FocusScreen</h1>
-      <input type="text" placeholder="nombre" className="form-control" />
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="nombre"
+        className="form-control"
+      />
       <button className="btn btn-outline-dark mt-4" onClick={handleClick}>
         Focus
       </button>
